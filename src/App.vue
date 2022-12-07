7fd57c2d6a2fec4e70627d68ae7362095d85f73e
otash8110 <otash_baxadirov@mail.ru>
2022-12-07 16:24:06 +0500

    final updates

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld ref="mainPageComponent" />
+      <HelloWorld ref="mainPageComponent"
+      :handle-lock-function="LockChangeRequest"
+      :handle-led-function="LedStatChangeRequest" />
     </v-main>
   </v-app>
 </template>
@@ -12,7 +14,11 @@ import mqtt from "mqtt/dist/mqtt.min";
 
 var topics = {
     RFID_Check: "RFID_Check",
-    RFID_Check_Result: "RFID_Check_Result"
+    RFID_Check_Result: "RFID_Check_Result",
+    DHT_Send: "DHT_Send",
+    DoorStateTopic: "Door_State",
+    LockStateTopic: "Lock_State",
+    LedStateTopic: "LED_State"
 };
 
 
@@ -25,7 +31,7 @@ export default {
 
   data: () => ({
     connection: {
-        host: "j9f09aa1-internet-facing-ef1d133022690295.elb.us-east-1.amazonaws.com",
+        host: "vfc64260-internet-facing-376ebf692d75cbe3.elb.us-east-1.amazonaws.com",
         protocol: "ws",
         clientId: "node-front-client",
         username: "ivanov",
@@ -53,18 +59,57 @@ export default {
           });
           this.client.on("message", (topic, message) => {
             console.log(`Received message ${message} from topic ${topic}`);
-            if (topic == topics.RFID_Check_Result) {
-              this.$refs.mainPageComponent.FetchAllEnterHistory();
+            switch (topic) {
+              case (topics.RFID_Check_Result):
+                this.$refs.mainPageComponent.FetchAllEnterHistory();
+                break;
+              case (topics.DHT_Send): 
+                this.$refs.mainPageComponent.UpdateTempAndHumidity(message);
+                break;
+              case (topics.DoorStateTopic):
+                this.$refs.mainPageComponent.UpdateDoorState(message);
+                break;
+              }
             }
-          });
+          );
 
           this.client.subscribe(topics.RFID_Check_Result, () => {
             console.log("subscribed to", topics.RFID_Check);
+          })
+
+          this.client.subscribe(topics.DHT_Send, () => {
+            console.log("subscribed to", topics.DHT_Send);
+          })
+
+          this.client.subscribe(topics.DoorStateTopic, () => {
+            console.log("subscribed to", topics.DoorStateTopic);
           })
         }
       } catch (error) {
         this.connecting = false;
         console.log("mqtt.connect error", error);
+      }
+    },
+
+    LockChangeRequest(lockState) {
+      var value;
+      if (lockState) {
+        value = "0";
+        this.client.publish(topics.LockStateTopic, value)
+      } else {
+        value = "2";
+        this.client.publish(topics.LockStateTopic, value);
+      }
+    },
+
+    LedStatChangeRequest(ledState) {
+      var value;
+      if (ledState) {
+        value = "1";
+        this.client.publish(topics.LedStateTopic, value)
+      } else {
+        value = "0";
+        this.client.publish(topics.LedStateTopic, value);
       }
     }
   },
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,119 @@
+<!-- eslint-disable vue/no-side-effects-in-computed-properties -->
 <template>
   <v-container>
     <v-row justify="center" class="flex-column">
       <v-col>
-        <v-card>
-        <v-card-title v-if="enterHistory">
-          Last entered: {{LastPersonEntered}}
-        </v-card-title>
-      </v-card>
+        <v-card v-if="enterHistory">
+            <template v-slot:title>
+              Last Visitor
+            </template>
+            <template v-slot:text>
+              Last entered: {{LastPersonEntered}}
+            </template>
+        </v-card>
       </v-col>
-
+      <v-col>
+        <v-row >
+          <v-col cols="12">
+            <v-card class="mx-auto"
+            >
+              <v-card-item>
+                <v-icon size="x-large" color="red">mdi-home-thermometer-outline</v-icon>
+                <v-card-title class="text-xs-h6 text-md-h5 text-lg-h4">
+                  Temperature
+                </v-card-title>
+              </v-card-item>
+              <v-card-text>
+                <Line
+                  id="my-chart-temp"
+                  :options="chartOptions"
+                  :data="ChartDataTemp"
+                  ref="bar"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+          
+          <v-col>
+            <v-card>
+              <v-card-item>
+                <v-icon size="x-large" color="blue">mdi-water-percent</v-icon>
+                <v-card-title class=" text-xs-h6 text-md-h5 text-lg-h4">
+                  Humidity
+                </v-card-title>
+              </v-card-item>
+              <v-card-text>
+                <Line
+                  id="my-chart-hum"
+                  :options="chartOptions"
+                  :data="ChartDataHum"
+                  ref="bar"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col cols="7">
+            <v-card>
+              <v-card-item>
+                <v-icon size="x-large" color="black">mdi-lock-open</v-icon>
+                <v-card-title>
+                  Door lock
+                </v-card-title>
+              </v-card-item>
+              <v-card-text>
+                <v-switch
+                v-model="lockState"
+                @change="HandleLock"
+                color="green"
+                :label="DoorSwitch" inset></v-switch>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="5">
+            <v-card>
+              <v-card-item>
+                <v-icon size="x-large" color="brown">mdi-door-open</v-icon>
+                <v-card-title>
+                  Door lock
+                </v-card-title>
+              </v-card-item>
+              <v-card-text class="text-center">
+                <v-btn stacked
+                color="green"
+                prepend-icon="mdi-lock-open" variant="flat" v-if="doorState">
+                  Door Open
+                </v-btn>
+                <v-btn stacked
+                color="red"
+                prepend-icon="mdi-lock" variant="flat" v-if="!doorState">
+                  Door Closed
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-card-item>
+                <v-icon size="x-large" color="yellow">mdi-lightbulb-on</v-icon>
+                <v-card-title>
+                  Kitchen Light
+                </v-card-title>
+              </v-card-item>
+              <v-card-text class="text-center">
+                <v-switch
+                v-model="ledState"
+                @change="HandleLed"
+                color="green"
+                :label="LedState" inset></v-switch>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
       <v-col>
         <v-table theme="dark">
         <thead>
@@ -40,18 +145,59 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import { Line } from "vue-chartjs";
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
+
 
 export default {
   name: 'HelloWorld',
+  components: {
+    Line
+  },
+
+  props: ['handleLockFunction', 'handleLedFunction'],
 
   data: () => ({
     member: "Mother",
     isMember: false,
-    enterHistory: null
+    enterHistory: null,
+    temperature: 0,
+    humidity: 0,
+    lockState: false,
+    ledState: false,
+    doorState: false,
+
+    timeLables: [],
+    dataTemp: [],
+    lablesHum: [],
+    dataHum: [],
+    chartOptions: {
+        responsive: true
+      },
+    chartLabelTemp: "Temperature",
+    chartItemBackgroundColorTemp: "#f87979",
+
+    chartLabelHum: "Humidity",
+    chartItemBackgroundColorHum: "#79c0f8"
   }),
 
   computed: {
+    ChartDataTemp() {
+        return {
+          labels: this.timeLables.slice(),
+          datasets: [ { data: this.dataTemp.slice(), label: this.chartLabelTemp, backgroundColor: this.chartItemBackgroundColorTemp }]
+      };
+    },
+
+    ChartDataHum() {
+      return {
+          labels: this.timeLables.slice(),
+          datasets: [ { data: this.dataHum.slice(), label: this.chartLabelHum, backgroundColor: this.chartItemBackgroundColorHum }]
+      };
+    },
     LastPersonEntered() {
       if (this.isMember) {
         var lastKey = Object.keys(this.enterHistory).pop();
@@ -81,6 +227,18 @@ export default {
       } else {
         return 0;
       }
+    },
+
+    DoorSwitch() {
+      return this.lockState ? "Lock is open" : "Lock is closed";
+    },
+
+    DoorState() {
+      return this.doorState ? true : false;
+    },
+
+    LedState() {
+      return this.ledState ? "Light is on" : "Light is off";
     }
   },
 
@@ -88,15 +246,66 @@ export default {
     await this.FetchAllEnterHistory();
   },
 
+  watch: {
+    ChartData() {
+      this.chartUpdateFlag = false;
+    },
+    temperature(newTemperature) {
+      if(newTemperature > 0) {
+        if (this.dataTemp.length > 4) {
+          this.dataTemp.shift();
+          this.timeLables.shift();
+
+          this.dataHum.shift();
+          this.lablesHum.shift();
+        }
+        var date = new Date();
+        var currentDate = date.toLocaleTimeString();
+        this.dataTemp.push(this.temperature);
+        this.dataHum.push(this.humidity);
+        this.timeLables.push(currentDate);
+      }
+    }
+  },
+
   methods: {
     async FetchAllEnterHistory() {
-      var fetchUrl = "http://localhost:3000/enterhistory";
+      var fetchUrl =  process.env.HistoryEnterURL || "http://localhost:3000/enterhistory";
       axios.get(fetchUrl).then(res => {
         this.enterHistory = res.data;
         this.isMember = true;
       })
 
       console.log("CALLED");
+    },
+    UpdateTempAndHumidity(message) {
+      var messageValue = message.toString();
+      const [temperature, humidity] = messageValue.split(",");
+
+      this.temperature = temperature;
+      this.humidity = humidity;
+
+    },
+
+    UpdateDoorState(message) {
+      var messageValue = message.toString();
+      console.log(messageValue);
+      switch (messageValue) {
+        case ("1"):
+          this.doorState = true;
+          break;
+        case ("0"):
+          this.doorState = false;
+          break;
+      }
+    },
+
+    HandleLock() {
+      this.handleLockFunction(this.lockState);
+    },
+
+    HandleLed() {
+      this.handleLedFunction(this.ledState);
     }
   }
 
